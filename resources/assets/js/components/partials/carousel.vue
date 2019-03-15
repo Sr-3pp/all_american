c3db5102dc3956f4e35c2ffe5c7e2911cf5553e6
Sr.3pp <jruiz@anzen.com>
2019-03-15 14:05:36 -0600

    carousel

--- resources/assets/js/components/partials/carousel.vue
+++ resources/assets/js/components/partials/carousel.vue
@@ -2,10 +2,10 @@
     <section class="carousel">
         <button @click="prev()" class="control prev"> < </button>
         <transition-group class="slides" :name="transition">
-            <article :key="slide" class="slide" v-for="(slide, index) in slides" v-if="active === index">
+            <article :class="{'active': content === index}" :key="slide.id" class="slide" v-for="(slide, index) in slides" v-if="active === index">
                 <img src="/img/default.jpg" alt="slide 1">
                 <div class="content">
-                    <p class="title">{{slide}}</p>
+                    <p class="title">{{slide.name}}</p>
                     <p class="text">
                         Text
                     </p>
@@ -19,7 +19,7 @@
 <script>
     export default {
         mounted(){
-
+            console.log(this.slides);
         },
         props: [
             'slides'
@@ -27,20 +27,31 @@
         data(){
             return {
                 active: 0,
-                transition: 'next'
+                transition: 'next',
+                content: 0
             }
         },
         methods: {
             next(){
                 if(this.active < this.slides.length-1){
+                this.content = false
                     this.transition = 'next'
                     this.active += 1;
+                    var este = this;
+                    setTimeout(function(){ 
+                        este.content = este.active;
+                    }, 1000);
                 }
             },
             prev(){
                 if(this.active > 0){
+                this.content = false;
                     this.transition = 'prev'
                     this.active -= 1;
+                    var este = this;
+                    setTimeout(function(){ 
+                        este.content = este.active;
+                    }, 1000);
                 }
             }
         }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-    <carousel :slides="['first', 'seccond', 'third']"></carousel>
+    <?php 
+        $slides = [
+            [
+                'id' => 1,
+                'name' => 'slide 1'
+            ],
+            [
+                'id' => 2,
+                'name' => 'slide 2'
+            ],
+        ];
+    ?>
+    <carousel :slides="{{json_encode($slides)}}"></carousel>
     <full-section background-img="/img/default.jpg"></full-section>
     <half-section></half-section>
     <section class="section">
