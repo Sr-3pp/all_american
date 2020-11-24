5c05e922da664090fac9aa0981071eba8a7795d2
sr.3pp <martin.ru@outlook.com>
2020-11-23 23:43:20 -0600

    volvemos a la normalidad

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 @extends('layouts.app')
 
 @section('content')
@@ -23,39 +22,10 @@
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-=======
-
-<div class="container">
-    <div class="row justify-content-md-center mt-5">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Register</div>
-                <div class="card-body">
-                    <form role="form" method="POST" action="{{ url('/register') }}">
-                        {!! csrf_field() !!}
-
-                        <div class="form-group row">
-                            <label class="col-lg-4 col-form-label text-lg-right">Name</label>
-
-                            <div class="col-lg-6">
-                                <input
-                                        type="text"
-                                        class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}"
-                                        name="name"
-                                        value="{{ old('name') }}"
-                                        required
-                                >
-                                @if ($errors->has('name'))
-                                    <div class="invalid-feedback">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </div>
-                                @endif
->>>>>>> 88a2655ce9ffa1c1a090bc2b2731a8e31feb061b
                             </div>
                         </div>
 
                         <div class="form-group row">
-<<<<<<< HEAD
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
@@ -66,29 +36,10 @@
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-=======
-                            <label class="col-lg-4 col-form-label text-lg-right">E-Mail Address</label>
-
-                            <div class="col-lg-6">
-                                <input
-                                        type="email"
-                                        class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
-                                        name="email"
-                                        value="{{ old('email') }}"
-                                        required
-                                >
-
-                                @if ($errors->has('email'))
-                                    <div class="invalid-feedback">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </div>
-                                @endif
->>>>>>> 88a2655ce9ffa1c1a090bc2b2731a8e31feb061b
                             </div>
                         </div>
 
                         <div class="form-group row">
-<<<<<<< HEAD
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
@@ -99,27 +50,10 @@
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-=======
-                            <label class="col-lg-4 col-form-label text-lg-right">Password</label>
-
-                            <div class="col-lg-6">
-                                <input
-                                        type="password"
-                                        class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
-                                        name="password"
-                                        required
-                                >
-                                @if ($errors->has('password'))
-                                    <div class="invalid-feedback">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </div>
-                                @endif
->>>>>>> 88a2655ce9ffa1c1a090bc2b2731a8e31feb061b
                             </div>
                         </div>
 
                         <div class="form-group row">
-<<<<<<< HEAD
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
@@ -131,29 +65,6 @@
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Register') }}
-=======
-                            <label class="col-lg-4 col-form-label text-lg-right">Confirm Password</label>
-
-                            <div class="col-lg-6">
-                                <input
-                                        type="password"
-                                        class="form-control{{ $errors->has('password_confirmation') ? ' is-invalid' : '' }}"
-                                        name="password_confirmation"
-                                        required
-                                >
-                                @if ($errors->has('password_confirmation'))
-                                    <div class="invalid-feedback">
-                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
-                                    </div>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-lg-6 offset-lg-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
->>>>>>> 88a2655ce9ffa1c1a090bc2b2731a8e31feb061b
                                 </button>
                             </div>
                         </div>
@@ -162,9 +73,5 @@
             </div>
         </div>
     </div>
-<<<<<<< HEAD
 </div>
 @endsection
-=======
-</div>
->>>>>>> 88a2655ce9ffa1c1a090bc2b2731a8e31feb061b
