da5b9450a96f5479d5d4d0fca0d58a875d0217d3
sr.3pp <martin.ru@outlook.com>
2020-11-24 20:54:34 -0600

    sliders

--- resources/js/n_components/partials/newEntry.vue
+++ resources/js/n_components/partials/newEntry.vue
@@ -69,71 +69,74 @@
             &:last-child{
                 margin-right: 0;
             }
-            .cover{
-                height: 75%;
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                overflow: hidden;
-                position: relative;
-                align-items: center;
-                flex-shrink: 0;
-                color: $dark;
-                background-color: #FFF;
-                    .buttons{
+            a{
+                max-height: 50%;
+                .cover{
+                    height: 75%;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    overflow: hidden;
+                    position: relative;
+                    align-items: center;
+                    flex-shrink: 0;
+                    color: $dark;
+                    background-color: #FFF;
+                        .buttons{
+                            position: absolute;
+                            bottom: 0;
+                            left: 0;
+                            width: 100%;
+                            padding: 2% 0;
+                            display: flex;
+                            justify-content: space-between;
+                            background-color: rgba(#000, .8);
+                            >*{
+                                flex: {
+                                    grow: 1;
+                                    shrink: 1;
+                                    basis: 48%;
+                                }
+                            }
+                            >.btn{
+                                border-radius: 0;
+                                &:first-child{
+                                    margin-right: 1%;
+                                }
+                                &:last-child{
+                                    margin-left: 1%;
+                                }
+                            }
+                        }
+                    img{
+                        width: 100%;
+                        height: auto;
+                    }
+                }
+                .s-text{
+                    padding: 5%;
+                    position: relative;
+                    &::after{
+                        content: '';
+                        width: 100%;
+                        height: 1px;
+                        background-color: #000;
                         position: absolute;
                         bottom: 0;
                         left: 0;
-                        width: 100%;
-                        padding: 2% 0;
-                        display: flex;
-                        justify-content: space-between;
-                        background-color: rgba(#000, .8);
-                        >*{
-                            flex: {
-                                grow: 1;
-                                shrink: 1;
-                                basis: 48%;
-                            }
-                        }
-                        >.btn{
-                            border-radius: 0;
-                            &:first-child{
-                                margin-right: 1%;
-                            }
-                            &:last-child{
-                                margin-left: 1%;
-                            }
-                        }
                     }
-                img{
-                    width: 100%;
-                    height: auto;
                 }
-            }
-            .s-text{
-                padding: 5%;
-                position: relative;
-                &::after{
-                    content: '';
-                    width: 100%;
-                    height: 1px;
-                    background-color: #000;
-                    position: absolute;
-                    bottom: 0;
-                    left: 0;
-                }
-            }
-            .content{
-                padding: 5%;
-                margin-bottom: 15%;
-                max-height: 500px;
-                overflow: auto;
-                &::-webkit-scrollbar{
-                    display: none;
-                }
-                [data-f-id]{
-                    display: none;
+                .content{
+                    padding: 5%;
+                    margin-bottom: 15%;
+                    max-height: 500px;
+                    overflow: auto;
+                    &::-webkit-scrollbar{
+                        display: none;
+                    }
+                    [data-f-id]{
+                        display: none;
+                    }
                 }
             }
             >.social{
